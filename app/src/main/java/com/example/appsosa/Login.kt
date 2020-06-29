9b666e6b985166916912aa3eaf8ec18299968219
AlejandraLujanSosa <54086013+AlejandraLujanSosa@users.noreply.github.com>
2020-06-28 23:07:22 -0300

    login

--- app/src/main/java/com/example/appsosa/Login.kt
+++ app/src/main/java/com/example/appsosa/Login.kt
@@ -1,14 +1,21 @@
 package com.example.appsosa
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
+
+    private val GOOGLE_SIGN_IN = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +28,23 @@ class Login : AppCompatActivity() {
         //analytics.logEvent("InitScreen", bundle)
 
 
-
-
         //Setup
         setup()
+        session()
+    }
+
+    private fun session() {
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+
+        if (email != null && provider != null) {
+
+            showHome(email, ProviderType.valueOf(provider))
+        }
+
+
     }
 
     private fun setup() {
@@ -35,15 +55,17 @@ class Login : AppCompatActivity() {
             if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
 
                 FirebaseAuth.getInstance()
-                    .createUserWithEmailAndPassword(emailEditText.text.toString(),
-                        passwordEditText.text.toString()).addOnCompleteListener {
+                    .createUserWithEmailAndPassword(
+                        emailEditText.text.toString(),
+                        passwordEditText.text.toString()
+                    ).addOnCompleteListener {
 
                         if (it.isSuccessful) {
                             showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
                         } else {
                             showAlert()
                         }
-              }
+                    }
             }
 
         }
@@ -51,7 +73,8 @@ class Login : AppCompatActivity() {
             if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
 
                 FirebaseAuth.getInstance()
-                    .signInWithEmailAndPassword(emailEditText.text.toString(),
+                    .signInWithEmailAndPassword(
+                        emailEditText.text.toString(),
                         passwordEditText.text.toString()
                     ).addOnCompleteListener {
 
@@ -64,6 +87,20 @@ class Login : AppCompatActivity() {
             }
         }
 
+        googleButton.setOnClickListener {
+
+            //Configuracion
+
+            val googleConf =
+                GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id)).requestEmail()
+                    .build()
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+
+        }
     }
 
     private fun showAlert() {
@@ -85,8 +122,47 @@ class Login : AppCompatActivity() {
         startActivity(homeIntent)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        if (requestCode == GOOGLE_SIGN_IN) {
+
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try{
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null) {
+
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+
+                        if (it.isSuccessful) {
+                            showHome(account.email ?: "", ProviderType.GOOGLE)
+                        } else {
+                            showAlert()
+
+                        }
+                    }
+                }
+
+            }catch(e: ApiException){
+                showAlert()
+            }
+
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
 
 
 
--- app/src/main/java/com/example/appsosa/Registro.kt
+++ app/src/main/java/com/example/appsosa/Registro.kt
@@ -1,5 +1,6 @@
 package com.example.appsosa
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,7 +8,8 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_registro.*
 
 enum class ProviderType{
-    BASIC
+    BASIC,
+    GOOGLE
 }
 class Registro : AppCompatActivity() {
 
@@ -21,6 +23,13 @@ class Registro : AppCompatActivity() {
         val email = bundle?.getString("email")
         val provider = bundle?.getString("provider")
         setup(email ?:"", provider ?:"")
+
+        //Guardado de datos
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
     }
 
     private fun setup(email: String, provider: String){
@@ -30,7 +39,16 @@ class Registro : AppCompatActivity() {
         provideTextView.text = provider
 
         jugarBtn.setOnClickListener {
+
+            //Borrado de datos
+
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
+
             FirebaseAuth.getInstance().signOut()
+            //onBackPressed()
             val intent: Intent = Intent(this, Perfil::class.java)
             startActivity(intent)
             finish()
